eae66ad53f9d3bdf5c1615bbcbee1eeff89657f6
Mradula <mradulammittal@gmail.com>
2023-04-12 18:28:24 +0530

    Creating the bag of words, i.e. the vocabulary matrix. This step involves cleaning up sentences (input), initializing the matrix and assigning 1 to the positions as per the conditions.

--- processor.py
+++ processor.py
@@ -19,3 +19,23 @@ def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
+
+#creating the bag of words
+def bow(sentence, words, show_details = True):
+    #tokenize and lemmatize aka clean_up_sentence
+    sentence_words = clean_up_sentence(sentence)
+    #initializing bag of words(matrix of n words)
+    bag =  [0]*len(words)
+    for s in sentence_words:
+        for index, word in enumerate(words):
+            if word == s:
+                # assign 1 if current word is in the vocabulary position
+                bag[index] = 1
+                if show_details:
+                    print("Found in bag: %s" % word)
+                    #used % in place of ,
+
+    return(np.array(bag))
+
+
+
