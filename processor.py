6578cb648902131eee793aaaf8f392aa77055672
Mradula <mradulammittal@gmail.com>
2023-04-12 18:35:22 +0530

    Adding predict_class() function. This function outputs a list of intents and the probabilities(of them being true).

--- processor.py
+++ processor.py
@@ -37,5 +37,18 @@ def bow(sentence, words, show_details = True):
 
     return(np.array(bag))
 
+def predict_class(sentence, model):
+    #filter out predictions below a threshold
+    p = bow(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25  #to avoid too much overfitting
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
 
+    #sort by strength of probability
+    results.sort(key=lambda x:x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    
+    return return_list
 
