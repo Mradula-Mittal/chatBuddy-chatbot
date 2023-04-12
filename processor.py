e93f41ac592f04dc9ea81fa5bf6b30ec7d5cdde9
Mradula <mradulammittal@gmail.com>
2023-04-12 18:19:50 +0530

    Defining a function to clean up the input sentences

--- processor.py
+++ processor.py
@@ -14,3 +14,8 @@ intents = json.loads(open('health_chat_intents.json', encoding='utf-8').read())
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
 
+#clean up the input sentences
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
